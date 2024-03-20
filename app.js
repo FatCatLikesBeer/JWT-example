7345a78e00fe4cc6ca20e710d3ff847b70ca3dbe
Billy Bagel <itisbilly@gmail.com>
2024-03-19 23:55:27 -0700

    Finished the tutorial

--- app.js
+++ app.js
@@ -22,11 +22,67 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api', (req, res) => {
+
+app.get('/api', (req, res) => {
   res.json({
     message: 'Welcome to the API!',
   });
 });
+
+app.post('/api/posts', verifyToken, (req, res) => {
+  jwt.verify(req.token, 'secretkey', (error, authData) => {
+    if (error) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        message: 'POST created...',
+        authData: authData,
+        token: req.token,
+      });
+    }
+  });
+});
+
+app.post('/api/login', (req, res) => {
+  // Mock user
+  const user = {
+    id: 1,
+    username: 'brad',
+    email: 'brad@gmail.com',
+  };
+
+  jwt.sign({ user: user }, 'secretkey', { expiresIn: '120s' }, (err, token) => {
+    res.json({
+      token: token,
+    });
+  });
+});
+
+// FORMAT OF TOKEN
+// Authorization: Bearer <access_token>
+
+// Verify Token
+function verifyToken(req, res, next) {
+  // Get auth header value
+  const barerHeader = req.headers['authorization'];
+
+  // Check if barerHeader is undefined
+  if (typeof barerHeader !== 'undefined') {
+    // Split at the space
+    const barer = barerHeader.split(' ');
+
+    // Get token from array
+    const barerToken = barer[1];
+
+    // Set the token
+    req.token = barerToken;
+    next();
+  } else {
+    // Forbidden
+    res.sendStatus(403);
+  }
+};
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
